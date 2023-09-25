c48dc6cd5e809c5e9a06ca837607a42c51b046f2
rmunate <raulunatecastro@MacBook-Pro-de-Raul.local>
2023-09-25 13:02:24 -0500

    Add Arrayable

--- src/Predefined/Additional/AdditionalHelpersArrays.php
+++ src/Predefined/Additional/AdditionalHelpersArrays.php
@@ -2,7 +2,20 @@
 
 namespace Rmunate\LaravelHelpers\Predefined\Additional;
 
+use Rmunate\LaravelHelpers\Predefined\Arrayable\Arrayable;
+
 trait AdditionalHelpersArrays
 {
-    //..
+    
+    /**
+     * Get a new Arrayable object from the given array or string.
+     *
+     * @param  string|array  $values
+     * @return \Rmunate\LaravelHelpers\Predefined\Arrayable\Arrayable
+     */
+    public static function of(string|array $values)
+    {
+        return new Arrayable($values);
+    }
+
 }
